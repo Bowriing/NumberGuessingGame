616a0f5b1a21b90d2f46acd684eeb4e90d0278cc
Elliott <91552870+Bowriing@users.noreply.github.com>
2025-07-25 22:14:15 +0100

    Implemented new random number algorithm - mersenne tittie twister

--- numberGuessingGame.cpp
+++ numberGuessingGame.cpp
@@ -2,6 +2,7 @@
 // Created by ebowring on 25/07/2025.
 //
 
+#include <random>
 #include <iostream>
 
 int main(){
@@ -10,39 +11,52 @@ int main(){
     int iNum = 0;
     int iGuess = 0;
     bool bGuessed = false;
-    int iAttempts = 0;
-    std::string sUsername = "";
+    int iAttempts = 1;
+    std::string sUsername;
 
     //Welcome player and get relevant information
-    std::cout << "Welcome to BOWRING's Number Guessing Game!\n" << "Please enter your username!" << std::endl;
+    std::cout << "Please enter your username!\n";
     getline(std::cin, sUsername);
+    std::cout << "Hello " << sUsername << ", Welcome to ebowring's Number Guessing Game!\n" << std::endl;
 
-    //Generate random number with time as seed for generation
-    srand(time(nullptr));
-    iNum = (rand() % 100) + 1;
+    //Generate random number -- Uses Mersenne tittie twister algorithm
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 100);
+    iNum = dis(gen);
+    std::cout << iNum << std::endl;
 
-    //Guessing loop - will max out after 15 attempts
-    while(!bGuessed && iAttempts < 15){
+    //Guessing loop
+    while(!bGuessed && iAttempts < 10){
+
         std::cout << "Please guess a number (1-100): \n";
         std::cin >> iGuess;
 
-        if (iGuess == iNum) {
-            std::cout << "You Guessed It!\n";
+        if (iGuess <= 0 || iGuess > 100) {
+            std::cout << "Exceeds random number boundaries - please choose between 1-100\n";
+        }
+
+        else if (iGuess == iNum) {
+            std::cout << "You Guessed It!\n" << "In a total of " << iAttempts << " attempts." << std::endl;
             bGuessed = true;
         }
 
         else if (iGuess > iNum) {
-            std::cout << "Too High! Try Again.\n";
+            std::cout << "Too High!\n";
         }
 
-        else {
-            std::cout << "Too Low! Try Again.\n";
+        else if (iGuess < iNum) {
+            std::cout << "Too Low!\n";
         }
 
         iAttempts++;
     }
 
+    //Fail Message
+    std::cout << "You have failed to guess in 15 attempts, you lose >:D" << "\n";
+
     //Save players information to leaderboard
+    //Need to implement using a scalable storage/database system - depending on if I want an online leaderboard or not
 
 
     return 0;
